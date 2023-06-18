25da902a309523f3a1dc9966220dd771d7c6bdde
Caroline Souza Camargo <carolinecamargo012@gmail.com>
2023-06-18 14:10:36 -0300

    exercicio05

--- aula02/exercicio05/src/main.rs
+++ aula02/exercicio05/src/main.rs
@@ -1,13 +1,15 @@
 //5) Faça um programa que crie um array com 10 elementos iniciados com valores gerados randomicamente.
+use rand::Rng;
 
 fn main() {
-    let tamanho = 10;
-    let mut vet_aleatorio: Vec<i32> = Vec::new();
-
-    for i in 0..tamanho{
-        let mut j: i32;
-        j = i;
-        vet_aleatorio.push(j);
-        print!("{}  ", vet_aleatorio[j]);
+    let tam_max = 10;
+    let mut vet: Vec<i32> = Vec::new();
+    let mut rng = rand::thread_rng();
+    
+    for _i in 0..tam_max{
+        vet.push(0);
+        let num: i32 = rng.gen_range(0..999);
+        vet[_i] = num
     }
+    println!("{:?}", vet);
 }
